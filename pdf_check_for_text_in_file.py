5205cda56dbe270297fa3fb729ebe646a72a8796
Stephen Genusa <github@genusa.com>
2016-06-16 19:57:48 -0500

    Update pdf_check_for_text_in_file.py

--- pdf_check_for_text_in_file.py
+++ pdf_check_for_text_in_file.py
@@ -1,8 +1,8 @@
 """
 Purpose:
   - Check to see if there is text on any page in a PDF file. If so, leave the 
-        PDF filealone. 
-    If text is not found, move the file to another directory to be OCR'd
+        PDF file alone. If text is not found, move the file to another directory
+        to be OCR'd
               
 Author: Stephen Genusa
    URL: http://development.genusa.com
